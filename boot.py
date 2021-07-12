85d9a84c38dde6adc6ec322e875c1e42952ab75f
Chirawat <chirawat.not@gmail.com>
2021-07-12 21:19:48 +0700

    update code

--- boot.py
+++ boot.py
@@ -1,13 +1,12 @@
-# Complete project details at https://RandomNerdTutorials.com
-
 try:
   import usocket as socket
 except:
   import socket
 
-from machine import Pin
+from machine import Pin, I2C
 import network
 import dht
+import ssd1306
 
 import esp
 esp.osdebug(None)
@@ -35,4 +34,11 @@ Y1 = Pin(5, Pin.OUT)
 Y2 = Pin(4, Pin.OUT)
 Y1.off()
 Y2.off()
+
+i2c = I2C(scl=Pin(4), sda=Pin(5), freq=100000)
+display = ssd1306.SSD1306_I2C(128, 64, i2c)
+
+display.text('Hello World', 0, 0, 1)
+display.show()
+
 
--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 import ujson
+import time
 
 def web_page():
   if led.value() == 1:
     gpio_state="ON"
   else:
     gpio_state="OFF"
-  
-  html = """<html><head> <title>ESP Web Server</title> <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" href="data:,"> <style>html{font-family: Helvetica; display:inline-block; margin: 0px auto; text-align: center;}
-  h1{color: #0F3376; padding: 2vh;}p{font-size: 1.5rem;}.button{display: inline-block; background-color: #e7bd3b; border: none; 
-  border-radius: 4px; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}
-  .button2{background-color: #4286f4;}</style></head><body> <h1>ESP Web Server</h1> 
-  <p>GPIO state: <strong>""" + gpio_state + """</strong></p><p><a href="/?led=on"><button class="button">ON</button></a></p>
-  <p><a href="/?led=off"><button class="button button2">OFF</button></a></p></body></html>"""
-  return html
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 80))
@@ -45,22 +37,26 @@ while True:
     conn.send('HTTP/1.1 200 OK\n')
     conn.send('Content-Type: text/html\n')
   if data != -1:
+    display.fill(0)
     Y1.on()
-    print('DHT')
     d.measure()
     humidity = d.humidity()
     temp = d.temperature()
+    display.text("Temperature:{}".format(temp), 0, 0, 1)
+    display.text("Humidity:{}".format(humidity), 0, 20, 1)
+    display.show()
+    time.sleep(1)
     
     #response = "temp={} humidity={}".format(temp, humidity)
+    #strTemp = "{2:2f}".format(temp)
+    #strHumidity = "{2:2f}".format(humidity)
     response = ujson.dumps({'temperature': temp, 'humidity': humidity})
     print( response )
     
     conn.send('HTTP/1.1 200 OK\n')
     conn.send('Content-Type: application/json\n')
     
-  #response = web_page()
-  
-  #conn.send('Content-Type: application/json\n')
+
   conn.send('Connection: close\n\n')
   
   conn.sendall(response)
@@ -69,3 +65,4 @@ while True:
 
 
 
+    
